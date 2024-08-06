3418fbcec7eb1cf23b557f71014690c6e13f1abb
Yeseung-Park <yspark8538@naver.com>
2024-08-06 18:03:30 +0900

    0805 IM

--- 2635.py
+++ 2635.py
@@ -1,26 +1,24 @@
 N = int(input())
 
-total_result = []
+total_result = []    # 최종 결과를 집어 넣어 줄 리스트
 
-for i in range(1, N+1):
-    result = []
-    result.append(N)
-    result.append(i)
+for i in range(1, N+1):    # 두 번째 숫자로 가능한 모든 경우에 대해서 진행
+    temp_result = [N, i]    # 첫 번째 숫자와 두 번째 숫자 먼저 넣어주기
 
-    for j in range(N):
-        next_num = result[j] - result[j+1]
-        if next_num >= 0:
-            result.append(next_num)
-        else:
-            total_result.append(result)
-            break
+    while True:
+        next_num = temp_result[-2] - temp_result[-1]    # 다음으로 들어갈 요소는 temp_result의 마지막에서 두 번째 요소와 마지막 요소의 차
+        if next_num >= 0:    # 만약 다음으로 들어갈 요소가 음수가 아니라면
+            temp_result.append(next_num)    # 정상적으로 집어넣기
+        else:    # 만약 음수라면
+            break    # 넣지말고 while문 빠져나가기
+    total_result.append(temp_result)    # 남은 temp_result는 결과로 total_result에 집어넣기
 
-max_len = 0
+max_len = 0    # total_result 내 요소 하나하나의 길이를 비교하기 위한 기준 값
 
 for arr in total_result:
-    if len(arr) > max_len:
-        max_len = len(arr)
-        result = arr
+    if len(arr) > max_len:    # 만약 길이가 최대값보다 길다면
+        max_len = len(arr)    # 최대값은 해당 요소의 길이
+        final_result = arr    # 결과는 해당 요소(리스트)
 
-print(len(result))
-print(*result)
+print(max_len)
+print(*final_result)
--- 2669.py
+++ 2669.py
@@ -1,13 +1,13 @@
 # 직사각형 네 개의 합집합의 면적 구하기
 
-arr = [list(map(int, input().split())) for _ in range(4)]
+arr = [list(map(int, input().split())) for _ in range(4)]    # 주어진 변수를 모두 하나의 리스트로 지정
 
 total_area = set()
 
 for i in range(4):
-    square = arr[i]
+    square = arr[i]    # 주어진 정보 하나씩 개별의 사각형을 의미
     for j in range(square[0], square[2]):
-        for k in range(square[1], square[3]):
-            total_area.add(f'({j}, {k})')
+        for k in range(square[1], square[3]):    # 이렇게 해주면 square가 포함하는 작은 사각형을 좌표로 지정해줄 수 있음
+            total_area.add(f'({j}, {k})')    # 작은 사각형을 집합에 넣어줌으로써 중복된 것 제거
 
-print(len(total_area))
+print(len(total_area))    # 개수 산출
