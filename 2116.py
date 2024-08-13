131b346399918ea4459481d1af94ecbb5dc02d7f
Yeseung-Park <yspark8538@naver.com>
2024-08-13 12:21:10 +0900

    0813 IM

--- 2116.py
+++ 2116.py
@@ -1,1 +1,11 @@
 # 도저히 감도 안 잡히네요!
+
+N = int(input())    # 주사위의 개수
+arr = [list(map(int, input().split())) for _ in range(N)]
+dices = []
+
+for dice in arr:
+    dices.append([(dice[0], dice[5]), (dice[1], dice[3]), (dice[2], dice[4])])
+
+print(dices)
+
